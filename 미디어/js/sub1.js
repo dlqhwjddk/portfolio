1477daa2edb55bf18723ac1bbd72ccd3bf40fa06
dlqhwjddk <dlqhwjddk427@naver.com>
2020-04-22 20:50:54 +0900

    .

--- 미디어/js/sub1.js
+++ 미디어/js/sub1.js
@@ -1,5 +1,21 @@
 window.addEventListener("DOMContentLoaded", function(){
     
-    
-    
+    var profile = document.querySelector(".profile");
+    var div = document.querySelectorAll(".right_area div");
+//    console.log(div);
+//    console.log(profile);
+   /* window.addEventListener("scroll", function(){
+        
+        var sTop = window.scrollY;
+        console.log(sTop);
+        
+        if(sTop > 2160){
+            profile.style.top = "2100px";
+        }else{
+            profile.style.top = sTop + "px";
+        }
+        
+        
+    });*/
+     
 });
