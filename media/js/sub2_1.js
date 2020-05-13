1ec7931981c7799e830e77e7fdac64daa983a049
hhd92 <hhd92@daum.net>
2020-05-13 21:18:20 +0900

    오빠가 해줌

--- media/js/sub2_1.js
+++ media/js/sub2_1.js
@@ -4,14 +4,16 @@ window.addEventListener("DOMContentLoaded", function(){
 /*$(function (){*/
     
     
-    var param = location.href.split('=')[1];
-    var jsonUrl = ['js/filming.json','js/academy.json','js/friend.json'];
+    var param = location.href.split('=')[1] || "0";
+    console.log(param);
+    var jsonUrl = ['js/sub2/filming.json','js/sub2/academy.json','js/sub2/friend.json'];
     
     var li = document.querySelectorAll(".category li a");
     var category = ['Filming Site','The Oscars 2020','Colleagues of His'];
 
     function select(param){
-        
+        var gallery = $(".gallery");
+        gallery.html("");
         $.ajax({
             url : jsonUrl[param],
             type : 'GET',
@@ -20,27 +22,28 @@ window.addEventListener("DOMContentLoaded", function(){
 
                 for(var i in data.event){
                     var imgNode = "<li><img src=" + data.event[i] + "></li>";
-                    $(".gallery").append(imgNode);
-                };
+                    gallery.append(imgNode);
+                }
 
             }
         });
-             
-    };
+
+    }
     select(param);
     
     li[param].classList.add("active");
     
-    for(var i=0; i<li.length; i++){
+    for(let i=0; i<li.length; i++){
         li[i].addEventListener("click", function(){
-            
+
             for(var j=0; j<li.length; j++){
                 li[j].classList.remove("active");
             }
-            
+
             this.classList.add("active");
-        });    
-    };
+            select(i);
+        });
+    }
      
     
 /*});*/
--- media/js/sub3.js
+++ media/js/sub3.js
@@ -3,19 +3,50 @@ window.addEventListener("DOMContentLoaded", function(){
     
     var figure = document.querySelectorAll(".container figure > p");
     var black = document.querySelector(".black");
+    var contents = black.firstElementChild;
     
     for(var i=0; i<figure.length; i++){
         figure[i].addEventListener("click", function(){
+            contents.innerHTML = "";
             black.classList.add("active");
+            $.ajax({
+                url : "js/sub3/antarctic_journal.json",
+                type : 'GET',
+                dataType : 'json',
+                success :function(data){
+                    var html = "";
+                    html += "<p><img src='" + data.image_main + "' alt=''></p>"
+                    html += "<div class='movie_text'>"
+                    var movieText = data.movie_text;
+                    html += "<h3>" + movieText.title_ko + "</h3>"
+                    html += "<small>" + movieText.title_en + "</small>"
+                    html += "<dl>"
+                    html += "<dt>개요</dt><dd>" + movieText.information + "</dd>"
+                    html += "<dt>등급</dt><dd>" + movieText.grade + "</dd>"
+                    html += "<dt>누적관객</dt><dd>" + movieText.views + "</dd>"
+                    html += "<dt>주요정보</dt><dd>" + movieText.remark + "</dd>"
+                    html += "</dl>"
+                    html += "</div>"
+                    html += "<div class='trailer'>"
+                    html += "<h4>TRAILER</h4>"
+                    html += "<ul>"
+                    var trailers = data.trailer;
+                    for (var i = 0; i < trailers.length; i++) {
+                        var trailer = trailers[i];
+                        html += "<li>"
+                        html += "<a href='" + trailer.link + "'><img src='" + trailer.image + "' alt=''></a>"
+                        html += "<p>" + trailer.text + "</p>"
+                        html += "</li>"
+                    }
+                    html += "</ul>"
+                    html += "</div>"
+                    html += "<a class='x' href='javascript:void(0)'><img src='img/icon/x.png' alt=''></a>"
+                    contents.innerHTML = html;
+                    $(".x").click(function () {
+                        black.classList.remove("active");
+                    })
+                }
+            });
         });
-    };
-    
-    var x = document.querySelector(".pop .x");
-    
-    x.addEventListener("click", function(){
-        black.classList.remove("active");
-    });
-    
-    
-    
+    }
 });
